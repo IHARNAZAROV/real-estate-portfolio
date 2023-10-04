af073590e93d11fbfa3e0915068056d7115327a7
IHARNAZAROV <grizley.tery@gmail.com>
2023-10-04 19:47:59 +0300

    fix responsive map

--- assets/js/main.js
+++ assets/js/main.js
@@ -245,4 +245,15 @@
   tick()
   setInterval(tick, 1000)
 
+  window.transitionToPage = function(href) {
+    document.querySelector('body').style.opacity = 0;
+    setTimeout(function() { 
+        window.location.href = href;
+    }, 2000);
+};
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    document.querySelector('body').style.opacity = 1;
+});
+
 })()
