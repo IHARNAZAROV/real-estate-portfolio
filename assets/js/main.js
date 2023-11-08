704996b0e8c64d39a4c9ce76cca101c71f61c2a5
IHARNAZAROV <grizley.tery@gmail.com>
2023-11-08 06:38:07 +0300

    change hero image

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,6 +29,36 @@
     }
   };
 
+
+  var options = {
+    accessibility: true,
+    prevNextButtons: false,
+    pageDots: true,
+    setGallerySize: true,
+    arrowShape: {
+      x0: 10,
+      x1: 60,
+      y1: 50,
+      x2: 60,
+      y2: 45,
+      x3: 15
+    }
+  };
+  
+  var carousel = document.querySelector('[data-carousel]');
+  var slides = document.getElementsByClassName('carousel-cell');
+  var flkty = new Flickity(carousel, options);
+  
+  flkty.on('scroll', function () {
+    flkty.slides.forEach(function (slide, i) {
+      var image = slides[i];
+      var x = (slide.target + flkty.x) * -1/3;
+      image.style.backgroundPosition = x + 'px';
+
+    });
+  });
+
+
   /* Easy on scroll event listener   */
   const onscroll = (el, listener) => {
     el.addEventListener("scroll", listener);
