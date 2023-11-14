9e7cbf4a3bc96e0a3a1798145c837f5ec1da4858
IHARNAZAROV <grizley.tery@gmail.com>
2023-11-14 06:44:54 +0300

    add slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,39 +32,6 @@
     }
   };
 
-  const sliderContainer = document.querySelector('.slider-container')
-  const slideRight = document.querySelector('.right-slide')
-  const slideLeft = document.querySelector('.left-slide')
-  const upButton = document.querySelector('.up-button')
-  const downButton = document.querySelector('.down-button')
-  const slidesLength = slideRight.querySelectorAll('div').length
-  
-  let activeSlideIndex = 0
-  
-  slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
-  
-  upButton.addEventListener('click', () => changeSlide('up'))
-  downButton.addEventListener('click', () => changeSlide('down'))
-  
-  const changeSlide = (direction) => {
-      const sliderHeight = sliderContainer.clientHeight
-      if(direction === 'up') {
-          activeSlideIndex++
-          if(activeSlideIndex > slidesLength - 1) {
-              activeSlideIndex = 0
-          }
-      } else if(direction === 'down') {
-          activeSlideIndex--
-          if(activeSlideIndex < 0) {
-              activeSlideIndex = slidesLength - 1
-          }
-      }
-  
-      slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
-      slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
-  }
-
-
 
   /* Testimonials slider   */
   new Swiper(".testimonials-slider", {
@@ -103,6 +70,16 @@
     });
   });
 
+
+// Hero slider JS
+$('.hero-slider').owlCarousel({
+  animateOut: 'flipOutX',
+  animateIn: 'fadeIn',
+  items:1,
+  loop:true,
+  nav:true,
+  dots: true,
+  })
 
   window.transitionToPage = function (href) {
     document.querySelector("body").style.opacity = 0;
